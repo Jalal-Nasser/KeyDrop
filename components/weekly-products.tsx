b595ceed1f70eaa871f2e6f04c9edc01ad27f593
Jalal-Nasser <jnasser1983@gmail.com>
2025-07-03 01:36:13 +0300

    [dyad] Fixed quick view dialog overflow and padding - wrote 1 file(s)

--- components/weekly-products.tsx
+++ components/weekly-products.tsx
@@ -100,7 +100,7 @@ export function WeeklyProducts({ limit = 8 }) {
       </div>
 
       <Dialog open={isQuickViewOpen} onOpenChange={handleOpenChange}>
-        <DialogContent className="sm:max-w-4xl p-0">
+        <DialogContent className="sm:max-w-4xl max-h-[90vh] overflow-y-auto">
           {selectedProduct && (
             <div className="grid grid-cols-1 md:grid-cols-2">
               <div className="p-6 flex items-center justify-center bg-gray-100 md:rounded-l-lg">
