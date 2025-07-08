bcd647b4d11fd283357041f94bc19a457572f08e
Jalal-Nasser <jnasser1983@gmail.com>
2025-07-08 13:10:47 +0300

    [dyad] Updated the Product interface to include sale-related properties. - wrote 1 file(s)

--- src/types/product.ts
+++ src/types/product.ts
@@ -6,4 +6,7 @@ export interface Product {
   image?: string | string[]
   is_digital?: boolean
   download_url?: string
+  sale_price?: number // Added
+  is_on_sale?: boolean // Added
+  sale_percent?: number // Added
 }
