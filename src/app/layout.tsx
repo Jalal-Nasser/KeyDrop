6a69b1cb5be06c21fa0a8e29e1c019cf764c4225
Jalal-Nasser <jnasser1983@gmail.com>
2025-07-09 14:36:11 +0300

    [dyad] Integrated Google Analytics and Google Tag Manager - wrote 1 file(s)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import Script from "next/script" // Import Script from next/script
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -9,8 +10,8 @@ import { Footer } from "@/components/footer"
 import { Toaster } from "@/components/ui/sonner"
 import { PayPalProvider } from "@/context/paypal-provider"
 import { CartProvider } from "@/context/cart-context"
-import { MobileNavBar } from "@/components/mobile-nav-bar" // Import the new component
-import { WishlistProvider } from "@/context/wishlist-context" // Import WishlistProvider
+import { MobileNavBar } from "@/components/mobile-nav-bar"
+import { WishlistProvider } from "@/context/wishlist-context"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,7 +20,7 @@ export const metadata: Metadata = {
   description:
     "DropsKey is your one-stop shop for digital keys and software licenses. We offer a wide range of products at competitive prices, with instant delivery and top-notch customer support.",
   icons: {
-    icon: '/panda.png', // Updated to panda.png
+    icon: '/panda.png',
   },
 }
 
@@ -28,9 +29,50 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const gaMeasurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+
   return (
     <html lang="en" suppressHydrationWarning>
+      {/* Google Tag Manager - Part 1 (Head) */}
+      {gtmId && (
+        <Script id="google-tag-manager-head" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${gtmId}');`}
+        </Script>
+      )}
       <body className={inter.className}>
+        {/* Google Tag Manager - Part 2 (NoScript) */}
+        {gtmId && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
+        )}
+        {/* Google Analytics */}
+        {gaMeasurementId && (
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
+            strategy="afterInteractive"
+          />
+        )}
+        {gaMeasurementId && (
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gaMeasurementId}');
+            `}
+          </Script>
+        )}
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -40,11 +82,11 @@ export default function RootLayout({
           <PayPalProvider>
             <SessionProvider>
               <CartProvider>
-                <WishlistProvider> {/* Wrap with WishlistProvider */}
+                <WishlistProvider>
                   <Header />
-                  <main className="flex-grow pb-[60px]">{children}</main> {/* Added padding-bottom */}
+                  <main className="flex-grow pb-[60px]">{children}</main>
                   <Footer />
-                  <MobileNavBar /> {/* Add the mobile navigation bar here */}
+                  <MobileNavBar />
                 </WishlistProvider>
               </CartProvider>
             </SessionProvider>
